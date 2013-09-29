f224bc6e0a6e5c54bcd4e1c4e5c16a1443f15051
blueplanet <erguolinge@gmail.com>
2013-09-29 15:58:19 +0900

    Reply modelを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130929055014) do
+ActiveRecord::Schema.define(version: 20130929064433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20130929055014) do
     t.datetime "updated_at"
     t.text     "description"
   end
+
+  create_table "replies", force: true do |t|
+    t.text     "body"
+    t.integer  "topic_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "replies", ["topic_id"], name: "index_replies_on_topic_id", using: :btree
 
   create_table "topics", force: true do |t|
     t.string   "title"
