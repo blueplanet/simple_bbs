10bf138182958d1f6f559890640b0d52e146c41b
blueplanet <erguolinge@gmail.com>
2013-11-05 17:45:48 +0900

    Viewを調整

--- spec/features/replies_spec.rb
+++ spec/features/replies_spec.rb
@@ -8,7 +8,7 @@ feature 'ゲストは、トピックの書き込みを見たい' do
     before { visit topic_path(topic) }
 
     scenario 'トピックページにアクセスすると、書き込み一覧が表示される' do
-      expect(page).to have_css '.panel-heading', text: "合計 #{topic.replies.count} 件"
+      expect(page).to have_css '.panel-heading', text: "合計 #{topic.replies.size} 件"
 
       topic.replies.each do |reply|
         expect(page).to have_css "#replies .reply .body", text: reply.body
